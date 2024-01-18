d71f595e2f53093e036ffa19570a36a7365056bb
Yernur Zholdygali <ernur.123456789.ernur@gmail.com>
2024-01-18 15:31:04 +0600

    soloutions

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log(123456789);
+function call(n) {
+  if (n.length === 0) return 0;
+
+  let sum = 0;
+  for (let i = 0; i < n.length; i++) {
+    sum += n[i];
+  }
+
+  return sum / n.length;
+}
+
+const numArr = [1, 2, 3, 4, 5];
+const result = call(numArr);
+console.log(result); 
